1a130e0f41488279ef0859f8a7cf700500d22eb7
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-04-18 00:13:52 +0300

    FileTree

---
+++ FileTree/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace FileMenu
+{
+    public partial class Form1 : Form
+    {
+        string pathToFolder = "C:\\";
+
+        List<string> FilesFormat = new List<string> { "",".JPG", ".DOC", ".TXT", "HTML" };
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                pathToFolder = folderBrowserDialog1.SelectedPath;
+                textBox2.Text = pathToFolder;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == ""||comboBox1.SelectedIndex==-1)
+            {
+                MessageBox.Show("Empty field used default");
+            }
+
+
+            listBox1.Items.Clear();
+            string[] astrFiles = Directory.GetFiles(pathToFolder);
+            if (textBox1.Text == "")
+            {
+              
+                // listBox1.Items.Add("Всего файлов: " + astrFiles.Length);
+                foreach (string file in astrFiles)
+                {
+                    if (file.ToUpper().EndsWith(FilesFormat[comboBox1.SelectedIndex]))
+                    {
+                        listBox1.Items.Add(file);
+                    }
+
+                }
+                if (listBox1.Items.Count == 0)
+                {
+                    listBox1.Items.Add("Nothing found");
+                }
+            }
+            else
+            {
+            
+               
+                foreach (string file in astrFiles)
+                {
+                    if (comboBox1.SelectedIndex != 0)
+                    {
+                        if (file.ToUpper().EndsWith(textBox1.Text + FilesFormat[comboBox1.SelectedIndex]))
+                        {
+                            listBox1.Items.Add(file);
+                        }
+                    }
+                    else
+                    {
+                        if (file.ToUpper().Contains(textBox1.Text))
+                        {
+                            listBox1.Items.Add(file);
+                        }
+                    }
+
+                }
+                if (listBox1.Items.Count == 0)
+                {
+                    listBox1.Items.Add("Nothing found");
+                }
+            }
+        }
+    }
+}
