06eae047c30ca5cb3a15b7f133244ae91c394af7
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-04-18 00:10:59 +0300

    Quiz

---
+++ Quiz2/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace Quiz2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.HotTrack = true;
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(369, 214);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.radioButton3);
+            this.tabPage1.Controls.Add(this.radioButton2);
+            this.tabPage1.Controls.Add(this.radioButton1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(361, 188);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(64, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Previous";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Question:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(23, 106);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Answer 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(136, 106);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(69, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Answer 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(260, 106);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(69, 17);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Answer 3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(276, 246);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Check";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 281);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Quiz2/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz2
+{
+    public partial class Form1 : Form
+    {
+
+
+        int page = 2;
+
+        int correct = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+           int[] CorrectAnswers= new int[3]{ 1,2,0};
+             
+            RadioButton[]radioButtons=new RadioButton[3];
+            CreateTab();
+            CreateTab();
+
+
+
+
+        }
+
+        private void CreateTab()
+        {
+            RadioButton[] radioButtons = new RadioButton[3];
+            Label label2 = new Label();
+            label2.Location = new Point(106, 10);
+            label2.Text = $"Question {page}:";
+
+
+            TabPage tabPage1 = new TabPage($"{page}");
+            radioButtons[0] = new RadioButton();
+            radioButtons[1] = new RadioButton();
+            radioButtons[2] = new RadioButton();
+
+
+            tabPage1.Controls.Add(label2);
+
+            int a = -77;
+            int i = 0;
+            foreach (var item in radioButtons)
+            {
+                tabPage1.Controls.Add(radioButtons[i]);
+                i++;
+                item.Location = new Point(a += 106, 106);
+
+                item.Text = $"Answer {i}";
+            }
+
+
+            tabControl1.Controls.Add(tabPage1);
+            page++;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex<tabControl1.TabPages.Count-1){
+                tabControl1.SelectedTab = tabControl1.TabPages[++tabControl1.SelectedIndex];
+                
+            }
+          
+            if(tabControl1.SelectedIndex == tabControl1.TabPages.Count - 1)
+            {
+                button1.Enabled = false;
+                button1.Visible = false;
+
+                button3.Visible = true;
+                
+            }
+         
+          
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex >= 1)
+            {
+                tabControl1.SelectedTab = tabControl1.TabPages[--tabControl1.SelectedIndex];
+            }
+            if (tabControl1.SelectedIndex != tabControl1.TabPages.Count - 1)
+            {
+                button1.Enabled = true;
+                button1.Visible = true;
+
+                button3.Visible = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+               
+                if (radioButton1.Checked == true)
+                {
+                    correct++;
+                }
+                int z = 0;
+                foreach (var item in tabControl1.TabPages[1].Controls)
+                {
+                    if (item as RadioButton != null)
+                    {
+
+                        RadioButton radioButton = (RadioButton)item;
+                        if (radioButton.Checked == true && z == 2)
+                        {
+                            correct++;
+                        }
+                        z++;
+                    }
+                }
+            z = 0;
+                foreach (var item in tabControl1.TabPages[2].Controls)
+                {
+                    if (item as RadioButton != null)
+                    {
+
+                        RadioButton radioButton = (RadioButton)item;
+                        if (radioButton.Checked == true && z == 0)
+                        {
+                            correct++;
+                        }
+                        z++;
+                    }
+                }
+
+
+                MessageBox.Show($"Correctness {correct}");
+            correct = 0;
+            }
+        }
+    
+}
