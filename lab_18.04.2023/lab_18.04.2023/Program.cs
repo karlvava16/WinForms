83f7d2dd83d90509b41bb86d6d733970d1069b58
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-04-18 23:43:53 +0300

    lab

---
+++ lab_18.04.2023/lab_18.04.2023/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    internal class Program
+    {
+        public static void MyThread(object x)
+        {
+            if(x is List<Person>)
+            {
+                foreach (var element in (List<Person>)x) Console.WriteLine(element);
+
+            }
+        }
+
+        class Person
+         {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public int Age { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread tr1 = new Thread(new ParameterizedThreadStart(MyThread));
+
+            List<Person> people = new List<Person>(new Person("Vlad", "Karl", 19));
+        }
+    }
+}
