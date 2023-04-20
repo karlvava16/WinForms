ebdddb29e38aa4d2c10eef27b5fd700991192567
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-04-20 11:49:22 +0300

    taskmanager

---
+++ cw_20.04.2023/cw_20.04.2023/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cw_20._04._2023
+{
+    public partial class Form1 : Form
+    {
+        Process process;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            process = new Process();
+            process.StartInfo.FileName = "notepad.exe";
+            process.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            process.Kill();
+        }
+    }
+}
---
+++ hw_20.04.2023/hw_20.04.2023/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hw_20._04._2023
+{
+    public partial class Form1 : Form
+    {
+        Process[] processes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            RefreshList();
+        }
+
+        void RefreshList()
+        {
+
+            processes = Process.GetProcesses();
+            listBox1.Items.Clear();
+            foreach (Process process in processes)
+            {
+                listBox1.Items.Add($"{process.ProcessName}\t\t\t\t{process.Id}");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RefreshList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Match match = Regex.Match(listBox1.SelectedItems[0].ToString(), @"(\w+\\t\\t\\t\\t)(\d+)");
+            MessageBox.Show(match.Groups[1].Value);
+
+            foreach (var process in processes)
+            {
+                if (process.Id == Convert.ToInt32(match.Groups[2].Value))
+                {
+                    process.Kill();
+                    break;
+                }
+            }
+            RefreshList();
+
+        }
+    }
+}
