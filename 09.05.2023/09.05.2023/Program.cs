6084078fdb156b6303ecfb24fea0eed60cd185b0
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-05-10 12:02:15 +0300

    some work

---
+++ 09.05.2023/09.05.2023/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork
+{
+    class Point
+    {
+        int x;
+        int y;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+            unsafe
+            {
+                int a = 10;
+                int* ptr = &a;
+                Console.WriteLine((long)a);
+            }
+        }
+
+    }
+}
